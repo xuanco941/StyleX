fa5a1513baa19610d58d9c42a2981f61eb878a88
xuanco941 <xuanco941@gmail.com>
2024-01-17 19:13:04 +0700

    save

--- StyleX/DTOs/DesignDTO.cs
+++ StyleX/DTOs/DesignDTO.cs
@@ -1,8 +1,16 @@
 ﻿namespace StyleX.DTOs
 {
-    public class GetNaterialsByPartProductModel
+    public class SaveDesignInfoModel
     {
-        public int cartItemID { get; set; }
+        public int designInfoIDID { get; set; } // nếu = 0 thì tạo mới, khác 0 thì cập nhật
+        public int cartItemID { get; set; } 
         public int productSettingID { get; set; }
+        public int DesignInfoID { get; set; }
+        public string DesignName { get; set; } = string.Empty; // tên của bộ phận trên quần áo
+        public string? Color { get; set; }
+        public string? ImageTexture { get; set; } //color image url
+        public double? TextureRotation { get; set; }
+        public double? TextureScale { get; set; }
+        public string? ImageMaterial { get; set; } // image url
     }
 }
